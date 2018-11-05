6fa764789de310087af491496f513f1e96185f93
jordyjonatan <jorjonna@gmail.com>
2018-11-05 12:03:54 +0700

    market

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -18,68 +18,80 @@
             * {
                 font-family: "Montserrat";
             }
+
+            body {
+                background-image: url('public/image/background.png');
+                background-position: center center;
+                background-repeat: no-repeat;
+            }
+
+            h1 {
+                text-align: center;
+                color: #fff;
+                padding-top: 80px;
+                letter-spacing: 3px;
+                font-weight: bold;
+            }
+
+            .logo {
+                margin: 30px;
+                text-align: center;
+            }
+
+            p {
+                text-align: center;
+                color: #fff;
+                font-size: 12px;
+                letter-spacing: 3px;
+            }
         </style>
     </head>
     <body>
         
         <div class="container-fluid">
-            <div class="row">
-                <img class="img-fluid" src="{{ asset('public/image/slider1.png') }}" alt="">
+
+            <h1>WELCOME TO IT REPUBLIC</h1>
+
+            <div class="logo">
+                <img class="img-responsive" src="{{ asset('public/image/itrepublic_logo.png') }}" alt="">
             </div>
 
-            <h3>Daftar sekarang</h3>
-            @if($errors->any())
-                <div class="alert alert-danger">{{$errors->first()}}</div>
-            @endif
-            <form action="{{ route('landingSave') }}" method="POST" enctype="multipart/form-data">
-                {{csrf_field()}}
-                
-                <div class="row">
-                    <div class="form-group col-md-6">
-                        <label for="">Nama Depan *</label>
-                        <input type="text" name="nama_depan" class="form-control" required>
+            <p>Reserve your spot now at www.itrepublic.id</p>
+
+            <div class="row" align-items-center>
+            <div class="col col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        {{-- <h3>Daftar sekarang</h3> --}}
+                        @if($errors->any())
+                            <div class="alert alert-danger">{{$errors->first()}}</div>
+                        @endif
+                        <form action="{{ route('landingSave') }}" method="POST" enctype="multipart/form-data">
+                            {{csrf_field()}}
+                            
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label for="">Nama Depan *</label>
+                                    <input type="text" name="nama_depan" class="form-control" required>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="">Nama Belakang *</label>
+                                    <input type="text" name="nama_belakang" class="form-control" required>
+                                </div>
+                            </div>
+            
+                            <div class="form-group">
+                                <label for="">Email *</label>
+                                <input type="email" name="email" class="form-control" required>
+                            </div>
+            
+                            <button type="submit" class="btn btn-danger col-sm-12 col-md-3">Subscribe</button>
+                        </form>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Nama Belakang *</label>
-                        <input type="text" name="nama_belakang" class="form-control" required>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Email *</label>
-                    <input type="email" name="email" class="form-control" required>
-                </div>
-
-                <button type="submit" class="btn btn-danger">Subscribe</button>
-            </form>
-
-
-            <div class="row" style="text-align: center;">
-                <div class="col-md-12">
-                    <img src="{{ asset('public/image/land1.jpg') }}" width="250px" height="180px" alt="">
-                    <h2 style="text-align:center; padding-top: 15px;">FIND</h2>
-                    <p>Post a job to tell us about your project. 
-                    We'll quickly match you with the right freelancers.</p>
-                </div>
-                <div class="col-md-12">
-                    <img src="{{ asset('public/image/land2.png') }}" width="250px" height="180px" alt="">
-                    <h2 style="text-align:center; padding-top: 15px;">HIRE</h2>
-                    <p>Browse profiles, reviews, and proposals then interview top candidates. 
-                    Hire a favorite and begin your project</p>
-                </div>
-                <div class="col-md-12">
-                    <img src="{{ asset('public/image/land3.jpg') }}" width="250px" height="180px" alt="">
-                    <h2 style="text-align:center; padding-top: 15px;">WORK</h2>
-                    <p>Use the IT Republic platform to chat, share files, and 
-                    collaborate from your desktop or on the go.</p>
-                </div>
-                <div class="col-md-12">
-                    <img src="{{ asset('public/image/land4.jpeg') }}" width="250px" height="180px" alt="">
-                    <h2 style="text-align:center; padding-top: 15px;">PAY</h2>
-                    <p>Invoicing and payments happen through IT Republic. 
-                    With IT Republic Protection, only pay for work you authorize.</p>
                 </div>
             </div>
+            </div>
+
         </div>
 
         <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
